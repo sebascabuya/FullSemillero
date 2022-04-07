b36f971ca55b6d93f65ffbc069b6826344ce541f
Sebastian Cabuya <juancabuya@cbit-online.com>
2022-04-07 16:49:28 -0500

    Creación del servicio post de la tabla ciudades de la base de datos

--- src/main/java/com/semillero/practicatotal/controllers/CiudadesController.java
+++ src/main/java/com/semillero/practicatotal/controllers/CiudadesController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +21,16 @@ public class CiudadesController {
 	@Autowired
 	private CiudadesRepository ciudadesRepository;
 	
-	//private CiudadesEntity ciudadesEntity;
+	private CiudadesEntity ciudadesEntity;
 	
 	@GetMapping("/listaciudades")
 	public List<CiudadesEntity> obtenerListaCiudades(){
 		return ciudadesRepository.findAll();
+	}
+	
+	@PostMapping(value="/ingresarciudades", consumes = "application/json")
+	public CiudadesEntity ingresarCiudades(@RequestBody CiudadesEntity ciudadesBody) {
+		ciudadesEntity = ciudadesRepository.saveAndFlush(ciudadesBody);
+		return ciudadesEntity;
 	}
 }
