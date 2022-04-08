1922c83de81140b7bbb956b21770dc67e497e986
Sebastian Cabuya <juancabuya@cbit-online.com>
2022-04-07 20:54:32 -0500

    Creación y prueba del servicio put para la tabla ciudades

--- src/main/java/com/semillero/practicatotal/controllers/CiudadesController.java
+++ src/main/java/com/semillero/practicatotal/controllers/CiudadesController.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +43,18 @@ public class CiudadesController {
 	public CiudadesEntity ingresarCiudades(@RequestBody CiudadesEntity ciudadesBody) {
 		ciudadesEntity = ciudadesRepository.saveAndFlush(ciudadesBody);
 		return ciudadesEntity;
+	}
+	
+	@PutMapping(value="/editarciudad/{ciudadId}", consumes = "application/json")
+	public ResponseEntity<CiudadesEntity> editarCiudad(@PathVariable(value="ciudadId") Integer ciudadId, @RequestBody CiudadesEntity ciudadesBody) {
+		Optional<CiudadesEntity> optionalCiudades = ciudadesRepository.findById(ciudadId);
+		if(optionalCiudades.isPresent()) {
+			optionalCiudades.get().setStrCodigoDaneCiudad(ciudadesBody.getStrCodigoDaneCiudad());
+			optionalCiudades.get().setStrNombreCiudad(ciudadesBody.getStrNombreCiudad());
+			optionalCiudades.get().setDepartamentosEntity(ciudadesBody.getDepartamentosEntity());
+			return ResponseEntity.status(HttpStatus.CREATED).body(ciudadesRepository.save(optionalCiudades.get()));
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
