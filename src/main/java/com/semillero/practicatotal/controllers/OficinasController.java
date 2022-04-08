d6e12209c9630e9200234e5df11782e5daebdb7e
Sebastian Cabuya <juancabuya@cbit-online.com>
2022-04-08 12:58:05 -0500

    Creacion y comprobacion del metodo put de la entidad de oficinas

--- src/main/java/com/semillero/practicatotal/controllers/OficinasController.java
+++ src/main/java/com/semillero/practicatotal/controllers/OficinasController.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +27,7 @@ public class OficinasController {
 	private OficinasRepository oficinasRepository;
 	
 	private OficinasEntity oficinasEntity;
+	private Optional<OficinasEntity> oficinasOptional;
 	
 	@GetMapping("/listaoficinas")
 	public List<OficinasEntity> obtenerListaOficinas() {
@@ -32,7 +36,7 @@ public class OficinasController {
 	
 	@GetMapping("/oficinabyid/{oficinaId}")
 	public OficinasEntity obtenerOficinaById(@PathVariable(value="oficinaId") Integer oficinaId){
-		Optional<OficinasEntity> oficinasOptional = oficinasRepository.findById(oficinaId);
+		oficinasOptional = oficinasRepository.findById(oficinaId);
 		return oficinasOptional.get();
 	}
 	
@@ -40,5 +44,19 @@ public class OficinasController {
 	public OficinasEntity ingresarOficina(@RequestBody OficinasEntity oficinasBody) {
 		oficinasEntity = oficinasRepository.saveAndFlush(oficinasBody);
 		return oficinasEntity;
+	}
+	
+	@PutMapping(value="/actualizaroficina/{oficinaId}", consumes="application/json")
+	public ResponseEntity<OficinasEntity> actualizarOficinas(@PathVariable(value="oficinaId") Integer oficinaId, @RequestBody OficinasEntity oficinasBody){
+		oficinasOptional = oficinasRepository.findById(oficinaId);
+		if(oficinasOptional.isPresent()) {
+			oficinasOptional.get().setStrCodigoOficina(oficinasBody.getStrCodigoOficina());
+			oficinasOptional.get().setStrNombreOficina(oficinasBody.getStrNombreOficina());
+			oficinasOptional.get().setStrDireccionOficina(oficinasBody.getStrDireccionOficina());
+			oficinasOptional.get().setCiudadesEntity(oficinasBody.getCiudadesEntity());
+			return ResponseEntity.status(HttpStatus.CREATED).body(oficinasRepository.save(oficinasOptional.get()));
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
