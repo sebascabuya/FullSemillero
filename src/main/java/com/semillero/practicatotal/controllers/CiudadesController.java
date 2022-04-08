33abd8ca56f28bd68f68c31d33d9f93a3e482312
Sebastian Cabuya <juancabuya@cbit-online.com>
2022-04-07 21:01:13 -0500

    Creación y prueba de el servicio delete de la entidad ciudades

--- src/main/java/com/semillero/practicatotal/controllers/CiudadesController.java
+++ src/main/java/com/semillero/practicatotal/controllers/CiudadesController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,5 +57,10 @@ public class CiudadesController {
 		} else {
 			return ResponseEntity.notFound().build();
 		}
+	}
+	
+	@DeleteMapping(value="/eliminarciudad/{ciudadId}")
+	public void eliminarCiudad(@PathVariable(value="ciudadId") Integer ciudadId) {
+		ciudadesRepository.deleteById(ciudadId);
 	}
 }
